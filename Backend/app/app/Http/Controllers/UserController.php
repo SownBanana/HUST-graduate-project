b3e60833db2572eaa8c2dd3585bf993d7942cfd1
Sownbanana <s.v.o.a.26@gmail.com>
2021-03-01 03:37:05 +0700

    add redis

---
+++ Backend/app/app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        return $request->email;
+    }
+}
